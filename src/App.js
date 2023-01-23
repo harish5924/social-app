9fe2426e8c3f0de5310fa33c409aaa7442a36b52
harish5924 <122254037+harish5924@users.noreply.github.com>
2023-01-23 07:29:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from './page/home/Home';
+// import Register from './page/register/Register';
+
+function App() {
+  return (
+    <div>
+      <Home/>
+      {/* <Register/> */}
+    </div>
+  );
+}
+
+export default App;
